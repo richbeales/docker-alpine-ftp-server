dd5d09bf0b2b09ca610979fa9593a16a4dd1742e
Andres Caicedo <73312784+AndresCdo@users.noreply.github.com>
2023-04-22 17:53:28 +0200

    Update start_vsftpd.sh
    Optimize Bash Script for Improved Performance

--- start_vsftpd.sh
+++ start_vsftpd.sh
@@ -1,60 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
-#Remove all ftp users
+# Remove all ftp users
 grep '/ftp/' /etc/passwd | cut -d':' -f1 | xargs -n1 deluser
 
-#Create users
-#USERS='name1|password1|[folder1][|uid1] name2|password2|[folder2][|uid2]'
-#may be:
+# Create users
+# USERS='name1|password1|[folder1][|uid1] name2|password2|[folder2][|uid2]'
+# e.g.
 # user|password foo|bar|/home/foo
-#OR
+# OR
 # user|password|/home/user/dir|10000
-#OR
+# OR
 # user|password||10000
 
-#Default user 'ftp' with password 'alpineftp'
+# Default user 'ftp' with password 'alpineftp'
 
-if [ -z "$USERS" ]; then
-  USERS="ftp|alpineftp"
-fi
+USERS="${USERS:-ftp|alpineftp}"
 
-for i in $USERS ; do
-    NAME=$(echo $i | cut -d'|' -f1)
-    PASS=$(echo $i | cut -d'|' -f2)
-  FOLDER=$(echo $i | cut -d'|' -f3)
-     UID=$(echo $i | cut -d'|' -f4)
+IFS=" "
+for i in $USERS; do
+    IFS="|" read -r NAME PASS FOLDER UID <<< "${i}"
 
-  if [ -z "$FOLDER" ]; then
-    FOLDER="/ftp/$NAME"
-  fi
+    FOLDER="${FOLDER:-/ftp/$NAME}"
 
-  if [ ! -z "$UID" ]; then
-    UID_OPT="-u $UID"
-  fi
+    UID_OPT=""
+    if [ -n "$UID" ]; then
+        UID_OPT="-u $UID"
+    fi
 
-  echo -e "$PASS\n$PASS" | adduser -h $FOLDER -s /sbin/nologin $UID_OPT $NAME
-  mkdir -p $FOLDER
-  chown $NAME:$NAME $FOLDER
-  unset NAME PASS FOLDER UID
+    echo -e "$PASS\n$PASS" | adduser -h "$FOLDER" -s /sbin/nologin $UID_OPT "$NAME"
+    mkdir -p "$FOLDER"
+    chown "$NAME:$NAME" "$FOLDER"
 done
 
+MIN_PORT="${MIN_PORT:-21000}"
+MAX_PORT="${MAX_PORT:-21010}"
 
-if [ -z "$MIN_PORT" ]; then
-  MIN_PORT=21000
-fi
-
-if [ -z "$MAX_PORT" ]; then
-  MAX_PORT=21010
-fi
-
-if [ ! -z "$ADDRESS" ]; then
-  ADDR_OPT="-opasv_address=$ADDRESS"
+ADDR_OPT=""
+if [ -n "$ADDRESS" ]; then
+    ADDR_OPT="-opasv_address=$ADDRESS"
 fi
 
 # Used to run custom commands inside container
-if [ ! -z "$1" ]; then
-  exec "$@"
+if [ -n "$1" ]; then
+    exec "$@"
 else
-  exec /usr/sbin/vsftpd -opasv_min_port=$MIN_PORT -opasv_max_port=$MAX_PORT $ADDR_OPT /etc/vsftpd/vsftpd.conf
+    exec /usr/sbin/vsftpd -opasv_min_port="$MIN_PORT" -opasv_max_port="$MAX_PORT" $ADDR_OPT /etc/vsftpd/vsftpd.conf
 fi
-
